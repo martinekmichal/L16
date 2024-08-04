80a6cec74ba41e33ca6d0eeab6ced7bb3af64dcf
martinekmichal <martinek.m.84@gmail.com>
2024-08-04 19:10:30 +0200

    integer test1

--- Ukol/Task1_IntegerTest.py
+++ Ukol/Task1_IntegerTest.py
@@ -1,17 +1,4 @@
-class IntegerSet:
-    def __init__(self, elements):
-        self.elements = set(elements)
-    def soucet(self):
-        return sum(self.elements)
-    def prumer(self):
-        if not self.elements:
-            raise ValueError("Prázdná pole")
-        return self.soucet() / len(self.elements)
-    def maximum(self):
-        if not self.elements:
-            raise ValueError("Prázdná pole")
-        return max(self.elements)
-    def minimum(self):
-        if not self.elements:
-            raise ValueError("Prázdná pole")
-        return min(self.elements)
+import unittest
+from Task1_Integerset import IntegerSet
+
+
